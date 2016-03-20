4aee4d2392d064c0a6b44bebea334f99050f873f
Helen See <helen.see@rutgers.edu>
2016-03-20 19:48:51 -0400

    from trump

---
+++ Spawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	// The Monster that should be spawned
+	public GameObject trashPrefab;
+	
+	// Spawn Delay in seconds
+	public float interval = 5;
+	
+	// Use this for initialization
+	void Start() {
+		InvokeRepeating("SpawnNext", interval, interval);
+	}
+	
+	void SpawnNext() {
+		Instantiate(trashPrefab, transform.position, Quaternion.identity);
+	}
+}
---
+++ TrashMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashMove : MonoBehaviour
+{
+    public float speed = 1f;
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        if (player)
+            GetComponent<NavMeshAgent>().destination = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(player.transform.position);
+        //transform.Rotate(Vector3.down, speed );
+    }
+
+    void Awake() { Destroy(gameObject, 20); }
+}
